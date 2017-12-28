f4030640a385b358520e3a4574a29d7675b9f69b
marco.roth@intergga.ch <marco.roth@intergga.ch>
2017-12-28 17:21:46 +0100

    add defaults for resetConfig.php

--- assets/resetConfig.php
+++ assets/resetConfig.php
@@ -2,13 +2,13 @@
 
 	include('../../../config/glancrConfig.php');
 
-	setConfigValue("sbb_station", "");
+	setConfigValue("sbb_station", "Basel SBB");
 	setConfigValue("sbb_lines", "");
 	setConfigValue("sbb_categories", "");
-	setConfigValue("sbb_time_to_station", "");
-	setConfigValue("sbb_limit", "");
-	setConfigValue("sbb_cols", "");
-	setConfigValue("sbb_cols_width", "");
+	setConfigValue("sbb_time_to_station", "0");
+	setConfigValue("sbb_limit", "5");
+	setConfigValue("sbb_cols", "type,final_station,departure,departure_in,platform,delay");
+	setConfigValue("sbb_cols_width", "20,50,20,10,10,10");
 	setConfigValue("reload", "1");
 
 	header("location: /config/");
